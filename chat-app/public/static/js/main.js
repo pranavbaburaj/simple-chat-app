3683fad68fdd3a1825aae08450d385046be70ee9
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-01-20 13:28:22 +0530

    CSS

--- chat-app/public/static/js/main.js
+++ chat-app/public/static/js/main.js
@@ -22,28 +22,13 @@ socket.on("chat", (data) => {
 })
 
 
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-
-modal.style.display = "block";
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+hello.addEventListener('keydown', function(e) {
+  if (e.keyCode == 13) {
+      socket.emit("chat", {
+        message : hello.value,
+        handle : "daat",
+      })
+      hello.value = ""
+      console.log(window.location.host)
   }
-}
-
+})
